1df2bf1be3bfacb52436524518b3cba3b520ece2
mikloshetzer-sketch <mikloshetzer@gmail.com>
2026-06-16 12:18:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+
   plugins: [react()],
-  base: '/migracio-terkep/'
-})
+
+  base: "/migracio-terkep/",
+
+  build: {
+
+    outDir: "dist",
+
+    assetsDir: "assets",
+
+    sourcemap: false
+
+  },
+
+  server: {
+
+    host: true,
+
+    port: 5173
+
+  }
+
+});
